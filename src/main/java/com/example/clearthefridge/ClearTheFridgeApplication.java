d466ea654a982d4039804465c56a8b6ad02cebe1
kimjungryuel <zldrktduf0927@naver.com>
2025-05-20 22:36:43 +0900

    first commit

---
+++ src/main/java/com/example/clearthefridge/ClearTheFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.clearthefridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearTheFridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearTheFridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clearthefridge/ClearTheFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clearthefridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearTheFridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
