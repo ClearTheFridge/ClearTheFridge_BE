4e7ee4f6530c467a9f1c50ac8b971ede49dee175
mink <frog050406@gmail.com>
2025-07-02 12:24:32 +0900

    BN-6 feat/Recipe Service&Recipe ServiceImpl

--- src/main/java/com/example/clearthefridge/domain/recipe/service/RecipeService.java
+++ src/main/java/com/example/clearthefridge/domain/recipe/service/RecipeService.java
@@ -1,4 +1,16 @@
 package com.example.clearthefridge.domain.recipe.service;
 
+import com.example.clearthefridge.domain.recipe.entity.Recipe;
+
+import java.util.List;
+
 public interface RecipeService  {
+
+    Long saveRecipe(Recipe recipe);
+    Recipe updateRecipe(Recipe recipe);
+    void deleteRecipe(Long id);
+    List<Recipe> searchByTitle(String title);
+    List<Recipe> searchByIngredient(String name);
+    List<Recipe> searchByUsername(String username);
+
 }
--- src/main/java/com/example/clearthefridge/domain/recipe/service/impl/RecipeServiceImpl.java
+++ src/main/java/com/example/clearthefridge/domain/recipe/service/impl/RecipeServiceImpl.java
@@ -1,12 +1,51 @@
 package com.example.clearthefridge.domain.recipe.service.impl;
 
+import com.example.clearthefridge.domain.recipe.entity.Recipe;
+import com.example.clearthefridge.domain.recipe.repository.FindRecipeRepository;
+import com.example.clearthefridge.domain.recipe.repository.RecipeRepository;
 import com.example.clearthefridge.domain.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepo;
+    private final FindRecipeRepository findRecipeRepo;
+
+    @Override
+    public Long saveRecipe(Recipe recipe) {
+        return recipeRepo.save(recipe).getRecipeId();
+    }
+
+    @Override
+    public Recipe updateRecipe(Recipe recipe) {
+        return recipeRepo.save(recipe);
+    }
+
+    @Override
+    public void deleteRecipe(Long recipeId) {
+        recipeRepo.deleteById(recipeId);
+
+    }
+
+    @Override
+    public List<Recipe> searchByTitle(String title) {
+        return findRecipeRepo.findByrecipeNameContaining(title);
+    }
+
+    @Override
+    public List<Recipe> searchByIngredient(String ingreName) {
+        return findRecipeRepo.findByIngredientName(ingreName)
+    }
+
+    @Override
+    public List<Recipe> searchByUsername(String userName) {
+        return findRecipeRepo.findByIngredientName(userName);
+    }
 }
