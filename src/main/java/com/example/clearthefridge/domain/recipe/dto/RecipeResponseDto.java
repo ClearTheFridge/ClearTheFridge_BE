9f15b6f5a1fa824c6d43de41580ac2782361b695
mink <frog050406@gmail.com>
2025-07-09 17:37:01 +0900

    BN-6 fix/RecipeResponseDto

--- src/main/java/com/example/clearthefridge/domain/recipe/dto/RecipeResponseDto.java
+++ src/main/java/com/example/clearthefridge/domain/recipe/dto/RecipeResponseDto.java
@@ -6,21 +6,95 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Builder
-public class RecipeResposeDto {
-    private Long id;
-    private String recipeName;
-    private String username;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public static RecipeResponseDto fromEntity(Recipe recipe) {
-        return RecipeResponseDto.builder()
-                .id(recipe.getId())
-                .recipeName(recipe.getRecipeName())
-                .username(recipe.getUser().getUsername())
-                .build();
+
+public class RecipeResponseDto {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Builder
+    public static class RecipeSummary{
+        private Long recipeId;
+        private String recipeName;
+
+
+        public static RecipeSummary fromEntity(Recipe recipe) {
+            return RecipeSummary.builder()
+                    .recipeId(recipe.getRecipeId())
+                    .recipeName(recipe.getRecipeName())
+                    .build();
+        }
     }
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Builder
+    public static class RecipeDetailDto{
+        private Long recipeId;
+        private String recipeName;
+        private String recipeContext;
+        private Long id; //userId
+        private String userName;
+        private List<IngredientDto> ingredients;
+        private List<ReviewDto> reviews;
+
+
+        @Getter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Builder
+        public static class IngredientDto {
+            private Long ingredientId;
+            private String ingredientName;
+            private String amount;
+        }
+
+        @Getter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Builder
+        private static class ReviewDto {
+            private Long reviewId;
+            private int rating;
+            private String comment;
+            private Long userId;
+            private String userName;
+        }
+        public static RecipeDetailDto fromEntity(Recipe recipe) {
+            List<IngredientDto> ings = recipe.getIngredients().stream()
+                    .map(ri -> IngredientDto.builder()
+                            .ingredientId(ri.getIngredient().getId())
+                            .ingredientName(ri.getIngredient().getName())
+                            .amount(ri.getAmount())
+                            .build())
+                    .collect(Collectors.toList());
+
+            List<ReviewDto> revs = recipe.getReviews().stream()
+                    .map(r -> ReviewDto.builder()
+                            .reviewId(r.getId())
+                            //댓글 개수는 제외
+                            .comment(r.getContent())
+                            .userId(r.getUser().getUserId())
+                            .userName(r.getUser().getUserName())
+                            .build())
+
+                    .collect(Collectors.toList());
+
+            return RecipeDetailDto.builder()
+                    .recipeId(recipe.getRecipeId())
+                    .recipeName(recipe.getRecipeName())
+                    .recipeContext(recipe.getRecipeContext())
+                    .userName(recipe.getUser().getUserName())
+                    .ingredients(ings)
+                    // 여기서 리스트 크기를 계산해서 채워줍니다.
+                    .reviews(revs)
+                    .build();
+        }
+    }
 }
+
+
+
